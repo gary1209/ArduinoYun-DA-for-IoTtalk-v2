dfc7408db8fcb982b6b6d3935a50ca7bf4a4db3d
Jyneda <jyneda@gmail.com>
2017-12-16 09:37:51 +0800

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+
+restart_period = 600
+
+cycle = 0
+timestamp = time.time()
+while True:
+    cycle = (cycle+1) % 10000
+    print('Cycle', cycle)
+    p = subprocess.Popen(['python', 'dai.py'])
+    while time.time()-timestamp < restart_period:
+        time.sleep(1)
+    subprocess.Popen.kill(p)
+    timestamp = time.time()
+
+    print
+    print('Restarting...') 
+
+
+
